2507578d991ac37a6c8ce0c39c2237a08bd6a728
JiJogbf <alex.gas1995@ya.ru>
2020-12-15 22:35:34 +0300

    Add Collection interface

---
+++ src/cpp/main/gas/coll/Collection.hpp
@@ -0,0 +1,19 @@
+#ifndef GAS_COLL_COLLECTION_HPP_15122020_223243
+#define GAS_COLL_COLLECTION_HPP_15122020_223243
+
+#include "..\Object.hpp"
+
+namespace gas{
+    namespace coll{
+        template<class T>
+        class Collection: public Iterable{
+        public:
+            virtual void add(T* a) = 0;
+            virtual int count() = 0;
+            virtual void remove(T* a) = 0;
+            virtual bool contains(T* a) = 0;
+        };
+    }
+}
+
+#endif 
