2c5c800df79891bc49e86dc0ef4e1e9d6c13c40e
JiJogbf <alex.gas1995@ya.ru>
2021-01-05 17:33:50 +0300

    adding doxygen comments to TestCaseDecorator.hpp

--- src/cpp/main/gas/test/TestCaseDecorator.hpp
+++ src/cpp/main/gas/test/TestCaseDecorator.hpp
@@ -4,12 +4,32 @@
 
 namespace gas{
     namespace test{
+
+        /**
+         * Decorator for TestCase.
+         * 
+         * Use when you need customize 
+         * existed casses with new logic
+        */
         class TestCaseDecorator: public TestCase{
         private:
             TestCase* mOrigin;
         public:
+            /**
+             * ctor
+             * 
+             * @param origin wrapped TestCase
+             * @param logger used logger
+            */
             TestCaseDecorator(TestCase* origin, Logger* logger);
+
+            /**
+             * dtor
+            */
             ~TestCaseDecorator() override;
+            /**
+             * entry point of test case
+            */
             void execute() override;
         };
     }
