e5e5cf547a5a8df46f4777f86f7e94d5f9625855
JiJogbf <alex.gas1995@ya.ru>
2021-01-05 16:41:26 +0300

    Adding doxygen comments to Ptr.hpp

--- src/cpp/main/gas/Ptr.hpp
+++ src/cpp/main/gas/Ptr.hpp
@@ -4,18 +4,53 @@
 #include "Object.hpp"
 
 namespace gas{
+
+    /**
+     * @brief Basic smart pointer in Core module
+    */
     template<class T>
     class Ptr: public Object{
     private:
         T* mPtr;
         int mCount;
     public:
+        /**
+         * @brief ctor
+         * 
+         * @param[in] ptr pointer with type of T
+        */
         Ptr(T* ptr);
+
+        /**
+         * @brief copy ctor
+         * 
+         * @param[in] p source pointer
+        */
         Ptr(Ptr& p);
+
+        /**
+         * @brief dtor
+        */
         ~Ptr() override;
+
+        /**
+         * @brief assigment operator
+        */
         Ptr& operator=(Ptr& p);
+
+        /**
+         * @brief the dereference operator
+        */
         T& operator*();
+
+        /**
+         * @brief member access operator overloading
+        */
         T* operator->();
+
+        /**
+        * @brief casting to pointer of holded pointer
+        */
         operator T*();        
     };
 }
