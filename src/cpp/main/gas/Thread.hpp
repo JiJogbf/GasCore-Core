ffeb0f9739e716e533827bfd7b8093c7061a318c
JiJogbf <alex.gas1995@ya.ru>
2021-01-05 16:54:09 +0300

    adding doxygen comments to Thread.hpp

--- src/cpp/main/gas/Thread.hpp
+++ src/cpp/main/gas/Thread.hpp
@@ -9,16 +9,49 @@ namespace gas{
         class ThreadImpl;
     }
 
+    /**
+     * @brief Class for working with threads
+    */
     class Thread: public Object{
     private:
         Task* mTask;
         impl::ThreadImpl* mImpl;
     public:
+        /**
+         * @brief ctor
+        */
         Thread();
+        
+        /**
+         * @brief ctor with specifyed task
+         * 
+         * @param task object with executable method wich 
+         * will be called after * start 
+        */
         Thread(Task* task);
+
+        /**
+         * dtor
+        */
         virtual ~Thread();
+
+        /**
+         * starting passed task in new thread
+        */
         void start();
+
+        /**
+         * wait until thread finish his job
+        */
         void join();
+
+        /**
+         * @brief main run cycle for thread
+         * 
+         * 
+         * @todo try move this method to private if not
+         * called anywhere
+        */
         virtual void run();
     };
 }
