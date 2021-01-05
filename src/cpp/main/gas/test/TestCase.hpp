aac3eb4646a264aa66b8f522f91185d9fc2bc283
JiJogbf <alex.gas1995@ya.ru>
2021-01-05 17:27:15 +0300

    adding doxygen for TestCase

--- src/cpp/main/gas/test/TestCase.hpp
+++ src/cpp/main/gas/test/TestCase.hpp
@@ -5,12 +5,38 @@
 
 namespace gas{
     namespace test{
+        /**
+         * @brief Base class for all test case's.
+         * 
+         * Used in Tester class. 
+         * 
+         * Inherit you test case from this class and implement execute
+         * method in derived instance.
+         * 
+         * Then pass this instance in add method of Tester class.
+         * 
+         * If you wana use logger you should first create it outside
+         * and pass it in ctor
+        */
         class TestCase: public Object{
         protected:
             Logger* mLogger;
         public:
+            /**
+             * @brief ctor
+            */
             TestCase(Logger* logger);
+
+            /**
+             * @brief dtor
+            */
             virtual ~TestCase() = 0;
+            
+            /**
+             * @brief main test case enty point.
+             * 
+             * You should implement it in derived testcase
+            */
             virtual void execute() = 0;
         };
     }
